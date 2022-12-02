e632c8262f6d550f3eaadbf905326e827232f0d4
ericparker14 <ericparker243@gmail.com>
2022-12-02 15:43:42 -0600

    Fix

--- addStudent.cpp
+++ addStudent.cpp
@@ -1,5 +1,43 @@
 #include <etp0040_header.h>
 
-void addStudent(){
-    cout << "Pls fix me" << endl;
+void add_Student(ofstream &outFS, Student newStudent){
+    int testScore;
+    // get all info
+    cout << "First name:";
+    cin >> newStudent.firstN;
+    cout << "Last name:";
+    cin >> newStudent.lastN;
+    cout << "Student ID:";
+    cin >> newStudent.studentID;
+    // create dyn array given # tests
+    cout << "# Tests taken:";
+    cin >> newStudent.testsTaken;
+    newStudent.ptr = new int(newStudent.testsTaken);
+    cout << "newStudent.ptr = " << *newStudent.ptr << endl;
+    // take in test scores input
+    cout << "Test scores:";
+    for (int i = 0; i < newStudent.testsTaken; i++){
+        cin >> testScore;
+        newStudent.ptr[i] = testScore;
+    }
+    // open file to write to
+    outFS.open("student.dat");
+    if(!outFS.is_open()){
+        cout << "File failed to open.";
+        exit(1);
+    }
+
+    outFS <<newStudent.lastN<<","<<newStudent.firstN<<","
+    <<newStudent.studentID<<","<<newStudent.testsTaken<<",";
+    for(int i = 0; i < newStudent.testsTaken; i++){
+        outFS << newStudent.ptr[i] << ",";
+    }
+    outFS.close();
+
+
+
+
+
+
+
 }
--- etp0040_header.h
+++ etp0040_header.h
@@ -1,6 +1,11 @@
+#ifndef  CODE_ETP0040_HEADER_H
+#define  CODE_ETP0040_HEADER_H
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
 #include <string>
-#include "getNumber.cpp"
 using namespace std;
+
+int getNumber();
+
+#endif // CODE_ETPOO40_HEADER.H
