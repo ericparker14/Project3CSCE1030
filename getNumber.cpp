e7b57b817ea327553b50a5b34b8348f512adcf45
ericparker14 <ericparker243@gmail.com>
2022-12-01 20:36:09 -0600

    fix

--- getNumber.cpp
+++ getNumber.cpp
@@ -20,24 +20,3 @@ int getNumber()
     return(count);
 }
 =======
-#include "euid_header.h"
-int getNumber()
-{
-ifstream fin;
-fin.open("student.dat");
-if (fin.fail())
-{
-cout<<"File error."<<endl;
-exit(1);
-}
-int count=0;
-string line;
-while(!fin.eof())
-{
-getline(fin, line);
-++count;
-}
-fin.close();
-return(count);
-}
->>>>>>> fdb8f4acdc889b3392d2d0a381370d3e9252bf68
